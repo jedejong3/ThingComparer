c2a85367376841aadeee88934d101a069d0416c1
avikboss <abosshar@macalester.edu>
2018-11-03 14:45:33 -0500

    Results page css

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -16,11 +16,13 @@ export class ResultsComponent {
   public winner;
   public loser;
   public response;
+  public appleWins: boolean;
 
   constructor(public navCtrl:NavController,navParams:NavParams) {
     this.winner = navParams.get('win');
     this.loser = navParams.get('loss');
     this.response = navParams.get('respond');
+    this.appleWins = Math.random() < 0.5;
   }
 
   onBackButton() {
