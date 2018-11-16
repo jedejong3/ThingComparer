b887d2602b4510324ed1d9ee7d23bf2a720fdf55
Jennifer DeJong <jdejong@macalester.edu>
2018-11-15 21:28:40 -0600

    fix up the backstory a bit

--- src/pages/backstory/backstory.ts
+++ src/pages/backstory/backstory.ts
@@ -6,26 +6,45 @@ import {NavController} from "ionic-angular";
   templateUrl: 'backstory.html'
 })
 export class BackStory {
+  private clickCount;
 
   constructor(public navCtrl: NavController) {
+    this.clickCount=0;
+
 
   }
   backButton() {
+    this.resetPage();
     this.navCtrl.pop();
   }
+  click(){
+    this.clickCount++;
+    if(this.clickCount==1) {
+      document.getElementById("image1").style.display = "none";
+      document.getElementById("image2").style.display = "visible";
+      document.getElementById("txt1").style.display = "none";
+      document.getElementById("txt2").style.display = "visible";
+      document.getElementById("txt3").style.display = "none";
+    }
+    if(this.clickCount==2) {
+      document.getElementById("image1").style.display = "none";
+      document.getElementById("image2").style.display = "none";
+      document.getElementById("txt1").style.display = "none";
+      document.getElementById("txt2").style.display = "none";
+      document.getElementById("txt3").style.display = "visible";
+    }
 
-   hide1() {
-    var x = document.getElementById("image1");
-    var y =document.getElementById("txt1");
-    y.style.display ="none";
-    x.style.display = "none";
 
   }
-  hide2() {
-    var x = document.getElementById("image2");
-    var y =document.getElementById("txt2");
-    y.style.display ="none";
-    x.style.display = "none";
+    resetPage()
+    {
+      document.getElementById("image1").style.display = "visible";
+      document.getElementById("image2").style.display = "visible";
+      document.getElementById("txt1").style.display = "visible";
+      document.getElementById("txt2").style.display = "visible";
+      this.clickCount=0;
 
-  }
+    }
+
+
 }
