8bb08aa6d276cfad1d7fd3e4ed95dcc7e7b1ef0d
avikboss <abosshar@macalester.edu>
2018-11-27 13:14:17 -0600

    grammar in adjective comparer

--- src/backend/comparers/adjective_comparer.ts
+++ src/backend/comparers/adjective_comparer.ts
@@ -1,6 +1,7 @@
 import {AbstractComparer} from "../abstract_comparer";
 import {Thing} from "../thing";
 import {Utilities} from "../utilities";
+import {plural} from 'pluralize'
 
 export class AdjectiveComparer extends AbstractComparer {
 
@@ -21,7 +22,7 @@ export class AdjectiveComparer extends AbstractComparer {
     if (winner.datamuseModified.length > 0) {
       for(var i =18 ; i<winner.datamuseModified.length; i++){
         if((winner.datamuseModified[i].tags=="n")){
-          response = 'I like ' + winner.name +" "+winner.datamuseModified[i].word+'. ';
+          response = 'I like ' + winner.name +" "+plural(winner.datamuseModified[i].word)+'. ';
           break;
         }
       }
