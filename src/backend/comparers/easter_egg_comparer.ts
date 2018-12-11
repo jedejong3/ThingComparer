b5d09659ee27bff8b821686347e0b21863f6cfdc
Nikhil <nsmith4@macalester.edu>
2018-12-11 14:02:35 -0600

    Added documentation for comparers.

--- src/backend/comparers/easter_egg_comparer.ts
+++ src/backend/comparers/easter_egg_comparer.ts
@@ -1,11 +1,14 @@
 import {AbstractComparer} from "../abstract_comparer";
 import {Thing} from "../thing";
 
+/**
+ * This class is designed to catch any 'special', pre-determined words and return a specific response for those words.
+ */
 export class EasterEggComparer extends AbstractComparer {
 
   compare(ThingOne: Thing, ThingTwo: Thing): string {
 
-
+    //If one of the 'things' entered satisfies any one of the below cases, our app will return the respective "special" response.
     if (this.hasEasterEgg("me", ThingOne, ThingTwo)) {
       return 'Stop trying to compare yourself: you are valuable just as you are!!'
     }
@@ -17,14 +20,13 @@ export class EasterEggComparer extends AbstractComparer {
       return "Y'know, I'm feeling conflicted on this one...it's almost like they're the same thing."
     }
 
-    if (this.hasEasterEgg("****",ThingOne,ThingTwo)) {
+    if (this.hasEasterEgg("****", ThingOne, ThingTwo)) {
       return "Chill with the words, homie."
     }
 
     if ((this.hasEasterEgg("apple", ThingOne, ThingTwo) || this.hasEasterEgg("apple", ThingOne, ThingTwo))
       && (this.hasEasterEgg("oranges", ThingOne, ThingTwo) || this.hasEasterEgg("orange", ThingOne, ThingTwo))
-  )
-    {
+    ) {
       return "Wow, aren't you original--who would ever have thought of comparing apples and oranges?";
     }
 
--- src/backend/comparers/stats_comparer.ts
+++ src/backend/comparers/stats_comparer.ts
@@ -3,9 +3,13 @@ import { Thing } from "../thing";
 import { Utilities } from "../utilities";
 import { plural } from 'pluralize'
 
+/**
+ * This class makes use of the 'statistically signifciant' call to Datamuses' API. A list of words that
+ * are statistically associated with the query word in the same piece of text is returned.
+ */
 export class StatsComparer extends AbstractComparer {
 
-  private lastResponseIndex = -1;
+  private lastResponseIndex = -1; //Tracks the last response.
 
   constructor() {
     super();
@@ -13,6 +17,7 @@ export class StatsComparer extends AbstractComparer {
 
   compare(ThingOne: Thing, ThingTwo: Thing): string {
     let winner;
+    //Sets data to be the datamuse data from the winning 'thing'.
     if (ThingOne.qualIndex > ThingTwo.qualIndex) {
       winner = ThingOne;
     } else {
@@ -23,17 +28,18 @@ export class StatsComparer extends AbstractComparer {
       return null;
     }
 
+    //Takes the highest scoring word and returns it as the most 'statistically significant'.
     if (winner.datamuseStats.length > 0) {
       for (var i = 0; i < winner.datamuseStats.length; i++) {
         if (winner.datamuseStats[i].score >= 0) {
-          var combined = `${winner.datamuseStats[i].word} ${plural(winner.name)}`;
           var responses = [
             `You know, when I think of "${plural(winner.name)}", I think of "${plural(winner.datamuseStats[i].word)}". And I really like thinking of 
             ${plural(winner.datamuseStats[i].word)}.`,
             `I just ran the numbers and apparently, "${plural(winner.name)}" is a trigger word for "${plural(winner.datamuseStats[i].word)}". LOVE it.`,
             `I can't hear "${winner.name}" without thinking "${plural(winner.datamuseStats[i].word)}". 
-            "${plural(winner.datamuseStats[i].word)}" are my favorite!`
+            "${plural(winner.datamuseStats[i].word).charAt(0).toUpperCase()}` + `${plural(winner.datamuseStats[i].word).slice(1)}"` + ` are my favorite!`
           ];
+
           //Picks a response from the array of responses, so long as the response isn't the same as the last random response.
           let responseIndex = Utilities.getRandomInt(responses.length);
           while (responseIndex == this.lastResponseIndex) {
