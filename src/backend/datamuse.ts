d3d8f8220c2cf6196d2cf85327a977eb5245c53c
Keara Berlin <kearaberlin@gmail.com>
2018-12-11 14:07:48 -0600

    handled errors from http request by resolving with an empty list as the data

--- src/backend/datamuse.ts
+++ src/backend/datamuse.ts
@@ -41,6 +41,10 @@ export class Datamuse {
           }
           resolve(data);
         };
+        request.onerror = function(error) {
+          console.log(error);
+          resolve([]);
+        }
         request.send();
       });
   }
